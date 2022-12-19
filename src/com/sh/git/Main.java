2601458b7d24b320673e0ca43937a4bd4ad1cb9f
yeji0401 <112843706+yeji0401@users.noreply.github.com>
2022-12-19 14:44:24 +0900

    원격저장소작업1

--- src/com/sh/git/Main.java
+++ src/com/sh/git/Main.java
@@ -15,6 +15,7 @@ public class Main {
 	
 	public void master() {
 		System.out.println("master!!!!!");
+		System.out.println("origin/master에서 작성함!!!!!");
 	}
 
 	public static void main(String[] args) {
