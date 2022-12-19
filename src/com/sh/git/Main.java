df2548eaa683575dea423a92882f757deee805da
yeji0401 <112843706+yeji0401@users.noreply.github.com>
2022-12-19 14:49:01 +0900

    원격저장소 작업22222

--- src/com/sh/git/Main.java
+++ src/com/sh/git/Main.java
@@ -16,6 +16,7 @@ public class Main {
 	public void master() {
 		System.out.println("master!!!!!");
 		System.out.println("origin/master에서 작성함!!!!!");
+		System.out.println("origin/master에서 작성함22222222222222222222!!!!!");
 	}
 
 	public static void main(String[] args) {
